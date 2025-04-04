c8934b7aa52f0e4ea974b5b9bbc19d044a9ae983
Marcele Monteiro <marcelepmonteiro@gmail.com>
2025-04-04 17:51:14 -0300

    Creates Book component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 
+import { useState } from "react";
+import Book from "@/components/Book";
+
 export default function Home() {
+  const [book, setBook] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-color-2 text-6xl font-title font-medium mb-8">
         BookFinder
       </h1>
 
-      <button className="cursor-pointer bg-color-2 text-color-1 text-sm font-sans rounded-lg uppercase p-4 hover:opacity-80 transition-all transition-discrete">
+      {book && <Book />}
+
+      <button
+        className="cursor-pointer bg-color-2 text-color-1 text-sm font-sans rounded-lg uppercase p-4 hover:opacity-80 transition-all transition-discrete"
+        onClick={() => setBook((prev) => !prev)}
+      >
         Gerar livro aleatório
       </button>
     </div>
---
+++ components/Book.tsx
@@ -0,0 +1,3 @@
+export default function Book() {
+  return <div className="mb-8">Book</div>;
+}
