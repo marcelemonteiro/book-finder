83314160c0115ab3e14e26d080706d5bd87d985c
Marcele Monteiro <marcelepmonteiro@gmail.com>
2025-04-07 11:15:55 -0300

    feat: Book component layout

--- components/Book.tsx
+++ components/Book.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
 export default function Book() {
-  return <div className="mb-8">Book</div>;
+  return (
+    <div className="md:flex md:gap-4 md:items-center p-4 mb-8 max-w-2xl bg-amber-100">
+      <Image src="/book_cover.png" width={150} height={150} alt="Book cover" />
+
+      <div className="flex flex-col gap-2">
+        <h2 className="text-xl font-medium">Jane Austen - Coleção I</h2>
+        <p className="text-sm">Jane Austen . 2020</p>
+        <p className="text-sm">
+          As obras de Jane Austen retratam personagens complexas da sociedade
+          rural georgiana e marcam a crescente do romantismo.
+        </p>
+      </div>
+    </div>
+  );
 }
