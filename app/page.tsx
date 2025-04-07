6c2de884ac4c1f4237431d87e2e4649b951b953a
Marcele Monteiro <marcelepmonteiro@gmail.com>
2025-04-07 11:16:15 -0300

    feat: add process activity icon

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Book from "@/components/Book";
+import Image from "next/image";
 
 export default function Home() {
   const [book, setBook] = useState(false);
@@ -12,7 +13,17 @@ export default function Home() {
         BookFinder
       </h1>
 
-      {book && <Book />}
+      {book ? (
+        <Book />
+      ) : (
+        <Image
+          src="/progress_activity.svg"
+          width={24}
+          height={24}
+          alt="Process activity icon"
+          className="animate-spin mb-8"
+        />
+      )}
 
       <button
         className="cursor-pointer bg-color-2 text-color-1 text-sm font-sans rounded-lg uppercase p-4 hover:opacity-80 transition-all transition-discrete"
